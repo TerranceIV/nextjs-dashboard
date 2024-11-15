7272d191d0aeea610458b44b3205fcb571ca42ed
Terrance <terrance@simsys.sg>
2024-11-15 11:13:54 +0800

    change up to chapter 5

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,20 @@ import styles from '@/app/ui/home.module.css';
 
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import NavLinks from '@/app/ui/dashboard/nav-links';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
+        
       </div>
+      <div className="flex justify-center gap-12">
+              <NavLinks />
+            </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/* Added for arrow */}
           <div className={styles.shape} />
@@ -30,6 +36,8 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
+
+
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
